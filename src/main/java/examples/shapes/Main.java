7178ca609009754f891d0498ed9032175360c5ac
Krista Gurney <gurneykri@gmail.com>
2018-10-09 20:08:45 -0600

    changed the testing in Main to have a function write to a file

--- src/main/java/examples/shapes/Main.java
+++ src/main/java/examples/shapes/Main.java
@@ -1,6 +1,10 @@
 package examples.shapes;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
+import java.io.IOException;
 
 public class Main {
 //    public static void main(String args[]){
@@ -70,16 +74,78 @@ public class Main {
 
     public static void main(String args[]){
         System.out.println("Hello Circle");
-        try{
+        List<Shape> shapeList = new ArrayList();
+
+        try {
             Point center = new Point(0, 1);
             double radius = 5;
             Circle circle = new Circle(center, radius);
+            shapeList.add(circle);
 
-            Stream s1 = circle.serialize();
-            Circle p2 = Circle.deserialize(s1);
-            System.out.println("Deserialized Circle =>" + p2.toString());
+            Point a = new Point(0, 0);
+            Point b = new Point(1, 0);
+            Point c = new Point(0, 1);
+            Triangle triangle = new Triangle(a, b, c);
+            shapeList.add(triangle);
+
+            Point topLeft = new Point(0, 1);
+            Point topRight = new Point(1, 1);
+            Point bottomLeft = new Point(0, 0);
+            Point bottomRight = new Point(1, 0);
+            Rectangle rectangle = new Rectangle(bottomLeft, bottomRight, topLeft, topRight);
+            shapeList.add(rectangle);
+
+            Point center2 = new Point(0, 1);
+            double a2 = 5;
+            double b2 = 3;
+            Ellipse ellipse = new Ellipse(center2, a2, b2);
+            shapeList.add(ellipse);
+
+            Point point1 = new Point(0, 1);
+            Point point2 = new Point(1, 1);
+            Line line = new Line(point1, point2);
+            shapeList.add(line);
+
         }catch(ShapeException e){
+                e.printStackTrace();
+        }
+        writeShapesToFile(shapeList,"test.txt");
+    }
+
+    public static void writeShapesToFile(List<Shape> shapeList, String fileName){
+        OutputStream fop = null;
+        File file;
+
+        try {
+            file = new File(fileName);
+
+            // if file doesnt exists, then create it
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            fop = new FileOutputStream(file);
+
+            for (Shape shape:shapeList) {
+                fop.write(shape.toString().getBytes());
+                fop.write("\n".getBytes());
+            }
+            fop.flush();
+            fop.close();
+            fop = null;
+            System.out.println("Done");
+
+        }catch(IOException e){
             e.printStackTrace();
+        }finally {
+            try {
+                if (fop != null) {
+                    fop.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
+
 }
