2968b1565f40c9594ac4e6a7b2ae19142857bafd
Krista Gurney <gurneykri@gmail.com>
2018-10-19 20:40:09 -0600

    finished testing for Square

--- src/test/java/examples/shapes/SquareTest.java
+++ src/test/java/examples/shapes/SquareTest.java
@@ -2,7 +2,10 @@ package examples.shapes;
 
 import org.junit.Test;
 
+import java.util.stream.Stream;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
 public class SquareTest {
@@ -31,6 +34,46 @@ public class SquareTest {
             fail("Non square coordinates should fail");
         }catch (ShapeException e){
             assertEquals(true,true);//expected this exception
+        }
+    }
+    @Test
+    public void testDeserialize(){
+        try {
+            String string = "<Square::bottomLeft=(<Point::x=0.0,y=0.0::Point>,bottomRight=<Point::x=1.0," +
+                    "y=0.0::Point>,topLeft=<Point::x=0.0,y=1.0::Point>,topRight=<Point::x=1.0,y=1.0::Point>::Square>";
+            Stream stream = string.codePoints().mapToObj(c -> String.valueOf((char) c));
+            Shape shape = Square.deserialize(stream);
+            Square square= (Square) shape;
+            assertNotNull(square);
+            assertEquals(square instanceof Rectangle, true);
+            assertEquals(square.getBottomLeft().getX() == 0.0, true);
+            assertEquals(square.getBottomLeft().getY() == 0.0, true);
+
+            assertEquals(square.getBottomRight().getX() == 1.0, true);
+            assertEquals(square.getBottomRight().getY() == 0.0, true);
+
+            assertEquals(square.getTopLeft().getX() == 0.0, true);
+            assertEquals(square.getTopLeft().getY() == 1.0, true);
+
+            assertEquals(square.getTopRight().getX() == 1.0, true);
+            assertEquals(square.getTopRight().getY() == 1.0, true);
+
+        }catch (ShapeException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testInvalidDeserialize(){
+        try {
+            String string = "<Square::bottomLeft=0.0::Point>,bottomRight=<Point::x=1.0," +
+                    "y=0.0::Point>,topLeft=<Point::x=0.0,y=1.0::Point>,topRight=<Point::x=1.0,y=1.0::Point>::Square>";
+            Stream stream = string.codePoints().mapToObj(c -> String.valueOf((char) c));
+            Shape shape = Square.deserialize(stream);
+            Square square = (Square) shape;
+            assertEquals(true, false);
+        }catch (ShapeException e){
+            assertEquals(true, true);
         }
     }
 }
